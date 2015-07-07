594091c1e4a603d055474e8a59dd380a7a094ca6
Carl && Katrina <carl&&katrina@appacademy.io>
2015-07-07 11:47:06 -0400

    Add template for toy list.

--- app/assets/javascripts/pokedex-1B.js
+++ app/assets/javascripts/pokedex-1B.js
@@ -15,11 +15,12 @@ Pokedex.RootView.prototype.renderPokemonDetail = function (pokemon) {
   this.$pokeDetail.html(content);
 
   // Phase 2C.
-  this.$pokeDetail.append(
-    '<span style="font-weight: bold;">Toys:</span><br>'
-  );
-  var $toys = $('<ul class="toys"></ul>');
-  this.$pokeDetail.append($toys);
+  // this.$pokeDetail.append(
+  //   '<span style="font-weight: bold;">Toys:</span><br>'
+  // );
+  // var $toys = $('<ul class="toys"></ul>');
+  // var $toys = this.$el(".toys");
+  // this.$pokeDetail.append($toys);
 
   pokemon.fetch({
     success: (function() {
